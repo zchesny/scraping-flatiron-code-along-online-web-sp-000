aac8e48289ac671ec2c82922ebcec4dbe19bd1e6
Zoey Chesny <z.chesny@gmail.com>
2020-04-16 19:01:29 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,9 +16,10 @@ class Scraper
       course.schedule = post.css(".date").text
       course.description = post.css("p").text
     end
+    binding.pry
   end
-  
-  binding.pry
+
+
 
   def get_courses
   end
