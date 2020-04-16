effa429aa26815cea3fb780a658cdf0431ebb078
Zoey Chesny <z.chesny@gmail.com>
2020-04-16 18:58:50 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,10 +9,10 @@ class Scraper
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
     doc.css(".post").each do |post|
-      course = Course.new 
+      course = Course.new
       course.title = post.css("h2").text
-      course.schedule = post.css('.date').text 
-      course.description = post.css("p").text 
+      course.schedule = post.css('.date').text
+      course.description = post.css("p").text
     end 
     binding.pry
   end
